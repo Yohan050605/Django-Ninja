e324dc291eb5b9fdf28df800330a6c47a5087abe
yohan050605 <dev.yohan05@gmail.com>
2023-12-06 11:01:41 +0900

    :sparkles: :: post 기능

--- ninjaTutorial/api.py
+++ ninjaTutorial/api.py
@@ -1,7 +1,16 @@
-from ninja import NinjaAPI
+from ninja import NinjaAPI, Schema
+from datetime import date
+from api.models import *
 
 api = NinjaAPI()
 
-@api.get("/hello")
-def hello(request):
-    return "Hello world"
+class EmployeeIn(Schema):
+    first_name: str
+    last_name: str
+    department_id:int = None
+    birthdate: date = None
+
+@api.post("/employees")
+def create_emplayee(requests, payload: EmployeeIn):
+    employee = Employee.objects.create(**payload.dict())
+    return {"id": employee.id}
