4da2abb3fec1a42ea0e93f6222d4309ee03c721f
yohan050605 <dev.yohan05@gmail.com>
2023-12-06 11:05:07 +0900

    :sparkles: :: get 기능

--- ninjaTutorial/api.py
+++ ninjaTutorial/api.py
@@ -1,10 +1,18 @@
 from ninja import NinjaAPI, Schema
 from datetime import date
 from api.models import *
+from django.shortcuts import get_object_or_404
 
 api = NinjaAPI()
 
-class EmployeeIn(Schema):
+class EmployeeIn(Schema): 
+    first_name: str
+    last_name: str
+    department_id:int = None
+    birthdate: date = None
+
+class EmployeeOut(Schema):
+    id: int
     first_name: str
     last_name: str
     department_id:int = None
@@ -14,3 +22,8 @@ class EmployeeIn(Schema):
 def create_emplayee(requests, payload: EmployeeIn):
     employee = Employee.objects.create(**payload.dict())
     return {"id": employee.id}
+
+@api.get("/emplayees/{employee_id}", response=EmployeeOut)
+def get_employee(requests, employee_id: int):
+    employee = get_object_or_404(Employee, id=employee_id)
+    return employee
